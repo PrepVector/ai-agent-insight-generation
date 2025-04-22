6c3614e56c7f48d213451d7b280b2d590b34f972
Shrimanta Satpati <82357659+shrimantasatpati@users.noreply.github.com>
2025-04-22 18:48:57 +0530

    Update custom_tool.py

--- tools/custom_tool.py
+++ tools/custom_tool.py
@@ -7,19 +7,6 @@ import seaborn as sns
 import statsmodels.api as sm
 from scipy import stats
 
-# class MyCustomTool(BaseTool):
-#     name: str = "Name of my tool"
-#     description: str = (
-#         "Clear description for what this tool is useful for, you agent will need this information to use it."
-#     )
-
-#     def _run(self, argument: str) -> str:
-#         # Implementation goes here
-#         return "this is an example of a tool output, ignore it and move along."
-
-
-
-
 class PythonREPLTool(BaseTool):
     """
     Custom tool to execute Python code and return the result.
